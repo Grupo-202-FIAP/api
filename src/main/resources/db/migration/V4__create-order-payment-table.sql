265ae79196d481b5c9da4c998a7969da3d86fa7b
leobr1t0 <leonardo.oliveira@sptech.school>
2025-05-27 00:33:58 -0300

    ajuste erros flyway

--- src/main/resources/db/migration/V4__create-order-payment-table.sql
+++ src/main/resources/db/migration/V4__create-order-payment-table.sql
@@ -7,22 +7,19 @@ CREATE TYPE payment_status AS ENUM (
 
 CREATE TYPE payment_method AS ENUM ('QRCODE');
 
-CREATE TABLE tb_order (
-    order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    total_price NUMERIC(10, 2) NOT NULL,
-    status order_status NOT NULL,
-    order_date_time TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    payment_fk_id UUID,
-    user_fk_id UUID,
-
-    CONSTRAINT fk_payment FOREIGN KEY (payment_fk_id) REFERENCES tb_payment(payment_id) ON DELETE SET NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_fk_id) REFERENCES tb_user(user_id) ON DELETE CASCADE
-);
-
-CREATE TABLE tb_payment(
+CREATE TABLE tb_payment (
     payment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     status payment_status NOT NULL,
     method payment_method NOT NULL,
     payment_date_time TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL
 );
 
+CREATE TABLE tb_order (
+    order_id SERIAL PRIMARY KEY,
+    total_price NUMERIC(10, 2) NOT NULL,
+    status order_status NOT NULL,
+    order_date_time TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    payment_fk_id UUID,
+
+    CONSTRAINT fk_payment FOREIGN KEY (payment_fk_id) REFERENCES tb_payment(payment_id) ON DELETE SET NULL
+);
