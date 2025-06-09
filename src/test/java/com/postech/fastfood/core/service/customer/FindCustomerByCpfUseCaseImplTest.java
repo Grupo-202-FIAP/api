0ec0dacac033c1235a7320864d802fd0d39c6db7
Fernandes <contatomatheusfernandeeess@gmail.com>
2025-06-09 13:57:10 -0300

    fix(tests): adjust mocks and assertions in CreateEmployee, FindCustomerByCpf, and ProductRepositoryAdapter tests

--- src/test/java/com/postech/fastfood/core/service/customer/FindCustomerByCpfUseCaseImplTest.java
+++ src/test/java/com/postech/fastfood/core/service/customer/FindCustomerByCpfUseCaseImplTest.java
@@ -1,24 +1,26 @@
 package com.postech.fastfood.core.service.customer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
 import com.postech.fastfood.core.domain.Customer;
 import com.postech.fastfood.core.domain.enums.UserRole;
-import com.postech.fastfood.core.ports.UserRepositoryPort;
+import com.postech.fastfood.core.ports.CustomerRepositoryPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class FindCustomerByCpfUseCaseImplTest {
-
     @Mock
-    UserRepositoryPort userRepositoryPort;
+    CustomerRepositoryPort customerRepositoryPort;
 
     @InjectMocks
     FindCustomerByCpfUseCaseImpl useCase;
@@ -35,7 +37,7 @@ class FindCustomerByCpfUseCaseImplTest {
         cliente.setName("João");
         cliente.setRole(role);
 
-        when(userRepositoryPort.findByCpf(cpf, role)).thenReturn(cliente);
+        when(customerRepositoryPort.findByCpf(cpf)).thenReturn(cliente);
 
         // Act
         Customer resultado = (Customer) useCase.execute(cpf);
@@ -45,7 +47,7 @@ class FindCustomerByCpfUseCaseImplTest {
         assertEquals(cpf, resultado.getCpf());
         assertEquals("João", resultado.getName());
         assertEquals(UserRole.ROLE_CUSTOMER, resultado.getRole());
-        verify(userRepositoryPort, times(1)).findByCpf(cpf, role);
+        verify(customerRepositoryPort, times(1)).findByCpf(cpf);
     }
 
     @Test
@@ -54,13 +56,13 @@ class FindCustomerByCpfUseCaseImplTest {
         String cpf = "00000000000";
         UserRole role = UserRole.ROLE_CUSTOMER;
 
-        when(userRepositoryPort.findByCpf(cpf, role)).thenReturn(null);
+        when(customerRepositoryPort.findByCpf(cpf)).thenReturn(null);
 
         // Act
         var resultado = useCase.execute(cpf);
 
         // Assert
         assertNull(resultado);
-        verify(userRepositoryPort, times(1)).findByCpf(cpf, role);
+        verify(customerRepositoryPort, times(1)).findByCpf(cpf);
     }
 }
