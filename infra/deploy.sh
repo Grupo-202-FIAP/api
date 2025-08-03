77c28b98eaf2eb097ee41d6a6774c19a8af59552
leobr1t0 <leonardo.oliveira@sptech.school>
2025-08-03 20:41:31 -0300

    adjjust manifests

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "🚀 Aplicando manifests da pasta k8s..."
+echo "📎 Iniciando cluster Minikube..."
+minikube start --cpus=4 --memory=8192
 
+echo "📊 Habilitando metrics-server para HPA..."
+minikube addons enable metrics-server
+
+echo "🔧 Configurando kubectl para usar o contexto Minikube..."
+kubectl config use-context minikube
+
+echo "🚀 Aplicando recursos da aplicação..."
 kubectl apply -f ./k8s --recursive
 
-echo "✅ Tudo aplicado com sucesso!"
+echo "✅ Deploy concluído!"
+kubectl get hpa
+kubectl get pods
